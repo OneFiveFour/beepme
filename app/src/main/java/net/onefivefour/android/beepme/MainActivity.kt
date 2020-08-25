5d5ece13bb8b2aa64c93871497004fb7c259ea4c
Florian Meyer <florian.meyer@onefivefour.net>
2020-08-25 23:56:28 +0200

    initial commit

---
+++ app/src/main/java/net/onefivefour/android/beepme/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.onefivefour.android.beepme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.onefivefour.android.beepme.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
